90f1f138f9d95fc4cf95c3e8a15504537dcf63a5
JuanPabloFrersCampos <juanpfrers@gmail.com>
2022-01-04 09:23:31 -0300

    permCheck > Counting elements

---
+++ codility-dev-training/4-counting-elements/permCheck.js
@@ -0,0 +1,99 @@
+/* A of N (non specified) intergers
+// Permutation = Sequence containing each element from 1 to N once, and only once.
+The goal is to check whether array A is a permutation.
+
+Task: Write a function: function solution(A);
+That, given an array A, returns 1 if array A is a permutation and 0 if it is not.
+
+Idea:
+Variable global arrayCopy;
+
+Desde una función extra:
+Inicializar arrayCopy[] desde el índice 0, hasta el índice A.length con valores false
+
+Desde función solution(A)
+Iterar element por elemento de A[], y:
+
+- En arrayCopy[element-1]:
+  Si dicho elemento del arreglo está en false, pasarlo a true.
+  Si dicho elemento del arreglo está en true, retornar 0 y terminar el programa (no es una permutación)
+
+- Una vez finalizada la iteración, revisar si alguno de los elementos en arrayCopy es false:
+  Retornar 0 si alguno es false
+  Returnar 1 si no.
+
+*/
+
+var arrayCopy = [];
+
+const initializeArray = maxIndex => {
+  for (let i=0; i < maxIndex; i++){
+    arrayCopy[i] = false;
+  }
+}
+
+function solution(A) {
+  initializeArray(A.length);
+  
+  for (element of A){
+    if (arrayCopy[element - 1] === false){
+      arrayCopy[element - 1] = true;
+      continue;
+    }
+    else{
+      return 0;
+    }
+  }
+
+  if (arrayCopy.some(element => element == false)) return 0;
+  else return 1;
+}
+
+console.log(solution(A));
+
+/*
+A non-empty array A consisting of N integers is given.
+
+A permutation is a sequence containing each element from 1 to N once, and only once.
+
+For example, array A such that:
+
+    A[0] = 4
+    A[1] = 1
+    A[2] = 3
+    A[3] = 2
+is a permutation, but array A such that:
+
+    A[0] = 4
+    A[1] = 1
+    A[2] = 3
+is not a permutation, because value 2 is missing.
+
+The goal is to check whether array A is a permutation.
+
+Write a function:
+
+class Solution { public int solution(int[] A); }
+
+that, given an array A, returns 1 if array A is a permutation and 0 if it is not.
+
+For example, given array A such that:
+
+    A[0] = 4
+    A[1] = 1
+    A[2] = 3
+    A[3] = 2
+the function should return 1.
+
+Given array A such that:
+
+    A[0] = 4
+    A[1] = 1
+    A[2] = 3
+the function should return 0.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [1..100,000];
+each element of array A is an integer within the range [1..1,000,000,000].
+*/
